1a4fdbf3d34c909de646791a84c23c01381efb5d
THARUNKUMAR M <tharunkumarm.23cse@kongu.edu>
2026-05-14 10:37:49 +0530

    Auto-save from TaskCode: 5/14/2026, 10:37:48 AM

--- Mini-Github/server.js
+++ Mini-Github/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect("", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
